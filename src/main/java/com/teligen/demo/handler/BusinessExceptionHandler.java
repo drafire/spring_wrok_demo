9b079576b146a93e7123a6c8803bdc536242800d
drafire <hyljljlj@163.com>
2020-06-04 11:44:18 +0800

    1、增加了请求参数为空的判断

--- src/main/java/com/teligen/demo/handler/BusinessExceptionHandler.java
+++ src/main/java/com/teligen/demo/handler/BusinessExceptionHandler.java
@@ -4,8 +4,10 @@ import com.teligen.demo.dto.ResultDTO;
 import com.teligen.demo.exception.BusinessException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Required;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -20,28 +22,33 @@ public class BusinessExceptionHandler {
      * 处理自定义异常
      */
     @ExceptionHandler(BusinessException.class)
-    public ResultDTO handleRRException(BusinessException e){
-        return ResultDTO.fail(e.getCode(),e.getMessage());
+    public ResultDTO handleRRException(BusinessException e) {
+        return ResultDTO.fail(e.getCode(), e.getMessage());
     }
 
     @ExceptionHandler(DuplicateKeyException.class)
-    public ResultDTO handleDuplicateKeyException(DuplicateKeyException e){
+    public ResultDTO handleDuplicateKeyException(DuplicateKeyException e) {
         logger.error(e.getMessage(), e);
         return ResultDTO.fail("数据库中已存在该记录");
     }
 
 
     @ExceptionHandler(Exception.class)
-    public ResultDTO handleException(Exception e){
+    public ResultDTO handleException(Exception e) {
+        if (e instanceof HttpMessageNotReadableException) {
+            return ResultDTO.fail("json参数不能为空");
+        }
+
         logger.error(e.getMessage(), e);
         return ResultDTO.fail();
     }
+
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResultDTO validationError(MethodArgumentNotValidException ex) {
         FieldError fieldError = ex.getBindingResult().getFieldError();
-        logger.error(fieldError.getField()+fieldError.getDefaultMessage());
-        return ResultDTO.fail(-99, fieldError.getField()+fieldError.getDefaultMessage());
+        logger.error(fieldError.getField() + fieldError.getDefaultMessage());
+        return ResultDTO.fail(-99, fieldError.getField() + fieldError.getDefaultMessage());
     }
 }
