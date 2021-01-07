b48cc2089b1856b33a0ed57ce28eff6a6a91b0c6
drafire <hyljljlj@163.com>
2021-01-07 22:15:46 +0800

    1、枚举值工具类新增重载方法，支持自定义方法校验，不只是getCode方法

--- src/main/java/com/teligen/demo/utils/EnumUtils.java
+++ src/main/java/com/teligen/demo/utils/EnumUtils.java
@@ -7,9 +7,10 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 /**
- * @author wayleung
+ * @author
  * @description 枚举工具类
  * @date 2020-06-08
+ * 必须要有getCode方法
  */
 public class EnumUtils {
     /**
@@ -18,16 +19,31 @@ public class EnumUtils {
      * @param clzz 枚举类 Enum
      * @param code
      * @return
-     * @author wayleung
+     * @author
      */
     public static boolean isInclude(Class<?> clzz, Object code)
+            throws InvocationTargetException,
+            IllegalAccessException, NoSuchMethodException {
+
+        return isInclude(clzz, code, "getCode");
+    }
+
+    /**
+     * 判断数值是否属于枚举类的值
+     *
+     * @param clzz 枚举类 Enum
+     * @param code
+     * @return
+     * @author
+     */
+    public static boolean isInclude(Class<?> clzz, Object code, String method)
             throws InvocationTargetException,
             IllegalAccessException, NoSuchMethodException {
 
         boolean include = false;
         if (clzz.isEnum()) {
             Object[] enumConstants = clzz.getEnumConstants();
-            Method getCode = clzz.getMethod("getCode");
+            Method getCode = clzz.getMethod(method);
             for (Object enumConstant : enumConstants) {
                 if (getCode.invoke(enumConstant).equals(code)) {
                     include = true;
@@ -38,19 +54,38 @@ public class EnumUtils {
         return include;
     }
 
+    /**
+     * @param clzz 枚举类 Enum
+     * @param code code
+     * @return 枚举类型父类
+     * @throws NoSuchMethodException
+     * @throws InvocationTargetException
+     * @throws IllegalAccessException
+     */
     public static Enum getEnum(Class<?> clzz, Object code) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        return getEnum(clzz, code, "getCode");
+    }
+
+    /**
+     * @param clzz 枚举类 Enum
+     * @param code code
+     * @return 枚举类型父类
+     * @throws NoSuchMethodException
+     * @throws InvocationTargetException
+     * @throws IllegalAccessException
+     */
+    public static Enum getEnum(Class<?> clzz, Object code, String method) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
         if (clzz.isEnum()) {
             Object[] enumConstants = clzz.getEnumConstants();
-            Method getCode = clzz.getMethod("getCode");
+            Method getCode = clzz.getMethod(method);
             for (Object enumConstant : enumConstants) {
                 if (getCode.invoke(enumConstant).equals(code)) {
                     return (Enum) enumConstant;
                 }
             }
-            throw new BusinessException(ResultCode.ENUM_CODE_NOT_EXIST.getCode(),ResultCode.ENUM_CODE_NOT_EXIST.getDes());
+            throw new BusinessException(ResultCode.ENUM_CODE_NOT_EXIST.getCode(), ResultCode.ENUM_CODE_NOT_EXIST.getDes());
         }
 
         throw new BusinessException(ResultCode.NOT_ENUM.getCode(), ResultCode.NOT_ENUM.getDes());
     }
-
 }
