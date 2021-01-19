2ba908ed2390513c4ddd0222feeca855155e7390
drafire <hyljljlj@163.com>
2021-01-19 10:18:22 +0800

    1、swagger-ui增加了token 参数输入框 2、拦截器不拦截OPTIONS的http请求

--- src/main/java/com/teligen/demo/configuration/Swagger2Config.java
+++ src/main/java/com/teligen/demo/configuration/Swagger2Config.java
@@ -4,12 +4,18 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -23,7 +29,15 @@ public class Swagger2Config {
      */
     @Bean
     public Docket createRestApi() {
+        ParameterBuilder ticketPar = new ParameterBuilder();
+        List<Parameter> params = new ArrayList<Parameter>();
+        ticketPar.name("token").description("警务token")
+                .modelRef(new ModelRef("string")).parameterType("header")
+                .required(true).build(); //header中的token参数必填
+        params.add(ticketPar.build());    //根据每个方法名也知道当前方法在设置什么参数
+
         return new Docket( DocumentationType.SWAGGER_2)
+                .globalOperationParameters(params)
                 .apiInfo(apiInfo())
                 .select()
                 //扫描需要展示的api接口
--- src/main/java/com/teligen/demo/configuration/interceptor/LoginInterceptor.java
+++ src/main/java/com/teligen/demo/configuration/interceptor/LoginInterceptor.java
@@ -5,6 +5,7 @@ import cn.hutool.core.util.StrUtil;
 import com.teligen.demo.configuration.JedisCache;
 import com.teligen.demo.dto.ResultCode;
 import com.teligen.demo.exception.BusinessException;
+import io.swagger.models.HttpMethod;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
@@ -22,38 +23,45 @@ public class LoginInterceptor extends HandlerInterceptorAdapter {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        String requestUrl = request.getRequestURL().toString();
-        if (!(requestUrl.endsWith("appLogin") || requestUrl.endsWith("appIdcardLogin")
-                || requestUrl.endsWith("/swagger-resources")
-                || requestUrl.endsWith("verifyAcode") || requestUrl.endsWith("/swagger-resources/configuration/ui"))) {//不拦截login
-            String token = request.getHeader("token");
-            //通过getHeaderNames获得所有头名字的Enumeration集合
-
-            Enumeration<String> headNames = request.getHeaderNames();
-
-            while (headNames.hasMoreElements()) {
-                String headName = headNames.nextElement();
-                //System.out.println(headName+":"+request.getHeader(headName));
-                logger.warn(headName + ":" + request.getHeader(headName));
-            }
-
-
-            if (StrUtil.isNullOrUndefined(token)) {
-                logger.warn("拦截掉" + request.getRemoteHost() + "的" + requestUrl + "请求:用户未登录");
-                throw new BusinessException(ResultCode.EMPTY_TOKEN.getCode(), ResultCode.EMPTY_TOKEN.getDes());
-            }
-
-            if (!jedis.existKey(JedisCache.JSESSIONID_PREFIX + token)) {
-                logger.warn("拦截掉" + request.getRemoteHost() + "的" + requestUrl + "请求:用户已断开连接");
-                throw new BusinessException(ResultCode.TOKEN_EXPIRE.getCode(), ResultCode.TOKEN_EXPIRE.getDes());
-            }
-
-            String userInfo = (String) jedis.get(token);
-            request.setAttribute("userInfo", userInfo);
-            return true;
-        } else {
+        //如果是option方法，则不拦截
+        if (HttpMethod.OPTIONS.toString().equalsIgnoreCase(request.getMethod())) {
             return true;
         }
-        //return true;
+
+        String requestUrl = request.getRequestURL().toString();
+        //白名单
+        if (requestUrl.endsWith("appLogin") || requestUrl.endsWith("appIdcardLogin")
+                || requestUrl.endsWith("/swagger-resources")
+                || requestUrl.endsWith("verifyAcode") || requestUrl.endsWith("/swagger-resources/configuration/ui")) {
+            return true;
+        }
+
+
+        String token = request.getHeader("token");
+        //通过getHeaderNames获得所有头名字的Enumeration集合
+
+        Enumeration<String> headNames = request.getHeaderNames();
+
+        while (headNames.hasMoreElements()) {
+            String headName = headNames.nextElement();
+            //System.out.println(headName+":"+request.getHeader(headName));
+            logger.warn(headName + ":" + request.getHeader(headName));
+        }
+
+
+        if (StrUtil.isNullOrUndefined(token)) {
+            logger.warn("拦截掉" + request.getRemoteHost() + "的" + requestUrl + "请求:用户未登录");
+            throw new BusinessException(ResultCode.EMPTY_TOKEN.getCode(), ResultCode.EMPTY_TOKEN.getDes());
+        }
+
+        if (!jedis.existKey(JedisCache.JSESSIONID_PREFIX + token)) {
+            logger.warn("拦截掉" + request.getRemoteHost() + "的" + requestUrl + "请求:用户已断开连接");
+            throw new BusinessException(ResultCode.TOKEN_EXPIRE.getCode(), ResultCode.TOKEN_EXPIRE.getDes());
+        }
+
+        String userInfo = (String) jedis.get(token);
+        request.setAttribute("userInfo", userInfo);
+        return true;
+
     }
 }
